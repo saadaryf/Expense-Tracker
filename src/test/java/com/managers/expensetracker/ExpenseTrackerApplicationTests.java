79cc9c572589f43401a37ba26a5953fcdb7fed91
Saad Arif <saadaryf@gmail.com>
2023-12-06 02:28:39 +0500

    initial commit

---
+++ src/test/java/com/managers/expensetracker/ExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.managers.expensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
